59b0615d0ad8cac7925eafb5c91ca9c2a1fe0e2d
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-03-24 06:31:46 +0300

    getters example

---
+++ redart/example/form.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+
+import 'package:redart/redart.dart';
+
+class Form with ControllerUtils {
+  @Re()
+  String _firstName = '';
+
+  @Re()
+  String _lastName = '';
+
+  @Re()
+  String _email = '';
+
+  @Re()
+  String _password = '';
+
+  String get name {
+    return '$firstName $lastName';
+  }
+
+  @Re()
+  bool get _firstNameValid {
+    return firstName.isNotEmpty;
+  }
+
+  @Re()
+  bool get _lastNameValid {
+    return lastName.isNotEmpty;
+  }
+
+  @Re()
+  bool get _emailValid {
+    return email.isNotEmpty && email.contains('@');
+  }
+
+  @Re()
+  bool get _passwordValid {
+    return password.isNotEmpty && password.length > 6;
+  }
+
+  @Re()
+  bool get _formValid {
+    return firstNameValid && lastNameValid && emailValid && passwordValid;
+  }
+
+  @override
+  String toString() {
+    return 'Form{firstName: $firstName, lastName: $lastName, email: $email, password: $password, valid: $formValid firstNameValid: $firstNameValid lastNameValid: $lastNameValid emailValid: $emailValid passwordValid: $passwordValid}';
+  }
+}
+
+void main() async {
+  final form = Form();
+  final dispose = listen(() {
+    print(form);
+  });
+
+  // input values
+  form.firstName = 'John';
+
+  // tick to auto update the form's reactive fields
+  await Future.delayed(Duration.zero);
+
+  // input all valid values
+  form.lastName = 'Doe';
+  form.email = 'john.doe@example.con';
+  form.password = 'password!!2';
+
+  // tick to auto update the form's reactive fields
+  await Future.delayed(Duration.zero);
+
+  // end
+  dispose();
+}
